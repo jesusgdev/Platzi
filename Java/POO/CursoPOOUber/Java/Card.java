a4ff55ec6e46b87cb38b41d7b512088d205581aa
jesusgdev <drakjdev@gmail.com>
2022-10-09 21:41:40 -0300

    Resubiendo repositorio de Platzi

---
+++ Java/POO/CursoPOOUber/Java/Card.java
@@ -0,0 +1,16 @@
+class Card extends Payment {
+
+    Integer id;
+    Integer number;
+    Integer cvv;
+    String date;
+
+    public Card(Integer id, Integer number, Integer cvv, String date) {
+        super(id);
+        this.number = number;
+        this.cvv = cvv;
+        this.date = date;
+
+    }
+
+}
